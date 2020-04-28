d09bc3f305eb17f9ce4e23812a5f8f1cc826ac1e
Sung M. Kim <sungmkim80@gmail.com>
2020-04-27 20:42:52 -0400

    Tried a material ui button, which worked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Button from '@material-ui/core/Button';
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -9,6 +11,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <Button variant="contained" color="primary" onClick={() => alert('hi!')}>
+      Hello World
+    </Button>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
