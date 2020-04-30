568d5313156164d92c9e5fdea6e48f658d8c07d7
Sung M. Kim <sungmkim80@gmail.com>
2020-04-30 01:55:23 -0400

    Initial import

---
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -0,0 +1,30 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+import themeData from "./light-theme";
+
+// const overrides = {
+//   // Style sheet name ⚛️
+//   MuiButton: {
+//     // Name of the rule
+//     text: {
+//       // Some CSS
+//       background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+//       borderRadius: 3,
+//       border: 0,
+//       color: "white",
+//       height: 48,
+//       padding: "0 30px",
+//       boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
+//     }
+//   }
+// };
+
+const theme = createMuiTheme(themeData)
+// const theme = createMuiTheme({
+//   ...themeData
+//   // overrides
+// });
+
+// console.info(`themeData`, themeData);
+
+export default theme;
