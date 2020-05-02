b555d8db872646f1ef99c3d51e16d61899f60744
Sung M. Kim <sungmkim80@gmail.com>
2020-05-02 02:14:27 -0400

    shows location

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { Router, useParams } from "@reach/router";
+import { Router, useParams, useLocation } from "@reach/router";
 import Typography from "@material-ui/core/Typography";
 
 import Layout from "../components/MiniDrawer";
 
 const PlayListItem = () => {
   const params = useParams();
+  const location = useLocation();
 
   React.useEffect(() => {
-    console.info(`params ==>`, JSON.stringify(params, null, 2))
+    console.info(`params ==>`, JSON.stringify(params, null, 2), `location=>`, location)
   }, [params]);
 
   return <Typography variant="title">PlayList</Typography>
